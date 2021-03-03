8f0fd3bf42890f74419777aef8a9562ab0e2e96f
cheredia2k12 <cheredia2k12@gmail.com>
2021-03-03 10:47:29 -0300

    aws instance

--- aws_ec2_instance.tf
+++ aws_ec2_instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "server1" {
     subnet_id = "${aws_subnet.main1.id}"
     associate_public_ip_address = true
     vpc_security_group_ids = ["${aws_security_group.sg1.id}", "${aws_security_group.sg2.id}"]
-
+    key_name = "${aws_key_pair.key-class1.id}"
 
 
     tags = {
@@ -13,4 +13,9 @@ resource "aws_instance" "server1" {
       "Owner" = "terraform"
       "Env" = "dev"
     }
+}
+
+output "public_dns" {
+    value = aws_instance.server1.*.public_dns
+  
 }
