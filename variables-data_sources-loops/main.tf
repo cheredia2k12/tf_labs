63735ffeadf4befbc51a7359ead8b3ceca1be2cc
cheredia2k12 <cheredia2k12@gmail.com>
2021-03-04 19:50:54 -0300

    agrego interpolacion loop data source para east-2

--- variables-data_sources-loops/main.tf
+++ variables-data_sources-loops/main.tf
@@ -26,3 +26,30 @@ resource "aws_subnet" "subnets" {
     "Name" = "Subnet-${count.index +1}"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main1.id}"
+
+  tags = {
+    "Name" = "VPC Main"
+  }
+}
+
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.main1.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    "Name" = "Public Subnet RT"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet" {
+  count = "${length(var.subnet1_cidr)}"
+  subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
