49892881deaaedecdfa9eeea149325e935cf960a
cheredia2k12 <cheredia2k12@gmail.com>
2021-03-04 12:15:04 -0300

    se comenta iam

--- aws_iam_access.tf
+++ aws_iam_access.tf
@@ -1,6 +1,6 @@
 #Si bien es posible crear roles, politicas, permisos por terraform, prefiero
-#crearlos a mano para tener mas control
-
+#hay que tener mucho cuidado con las definiciones y los permisos declarados.
+/*
 resource "aws_iam_group" "admins" {
     name = "Administradores"
 }
@@ -27,3 +27,28 @@ resource "aws_iam_group_membership" "admins-users" {
     ]
     group = "${aws_iam_group.admins.name}"
 }
+
+resource "aws_iam_access_key" "admin1-access" {
+    user = "${aws_iam_user.admin1.name}"
+}
+
+resource "aws_iam_access_key" "admin2-access" {
+    user = "${aws_iam_user.admin2.name}"
+}
+
+output "admin1_access_key" {
+    value = "${aws_iam_access_key.admin1-access.id}"
+}
+
+output "admin1_secret_key" {
+    value = "${aws_iam_access_key.admin1-access.secret}"
+}
+
+output "admin2_access_key" {
+    value = "${aws_iam_access_key.admin2-access.id}"
+}
+
+output "admin2_secret_key" {
+    value = "${aws_iam_access_key.admin2-access.secret}"
+}
+*/
