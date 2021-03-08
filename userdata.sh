7f4cb413a02982e9f52c991436fe9cd82770c905
cheredia2k12 <50061186+cheredia2k12@users.noreply.github.com>
2021-03-08 16:41:48 -0300

    Delete userdata.sh

--- userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/local/bin
-sudo -i
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-yum install -y httpd
-echo "hola mundo!!!!" > /var/www/html/index.html
-systemctl start httpd
-systemctl enable httpd
-echo "test_datasource" > hola.txt
